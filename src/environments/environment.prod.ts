cbfc982308225cdbcaebab72cb44f785f318067f
Jacek Jankowski <jacek.jankowski@stxnext.pl>
2018-02-07 18:26:22 +0100

    Dev config exends prod config.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAWZJm6aqnOVcYNJTJBfECE9ZBmHVNrvxI",
+    authDomain: "awesome-conf.firebaseapp.com",
+    databaseURL: "https://awesome-conf.firebaseio.com",
+    projectId: "awesome-conf",
+    storageBucket: "awesome-conf.appspot.com",
+    messagingSenderId: "994064671021"
+  },
 };
